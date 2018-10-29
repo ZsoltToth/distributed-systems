ca659ed77848ea78f3b291e148b77b4174336e49
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2018-10-29 13:15:10 +0100

    tsp console app has been initialized.

---
+++ tsp/console/src/main/java/hu/iit/uni/miskolc/distributed/tsp/App.java
@@ -0,0 +1,13 @@
+package hu.iit.uni.miskolc.distributed.tsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
